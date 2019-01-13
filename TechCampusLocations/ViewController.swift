223cb3f1015cdcfcc9c5ca0bb9e834a9794f4b5f
razan al ali <techcampus@razans-macbook-pro.local>
2019-01-13 16:36:56 +0200

    Initial Commit

---
+++ TechCampusLocations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechCampusLocations
+//
+//  Created by TechCampus on 1/13/19.
+//  Copyright © 2019 TechCampus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
